8c1add849f4a85468bf93d8b5624abe275cc96c1
domteoh <domteoh@gmail.com>
2020-09-13 16:45:41 +0800

    Subscript First Push

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+
+
+$(window).scroll(function () {
+  console.log($(window).scrollTop())
+    if ($(window).scrollTop() >= 130) {
+      $('#nav').addClass("sticky");
+      $('#banner').addClass("banner-sticky");
+      $('.nav-logo').fadeIn();
+    } else {
+      $('#nav').removeClass("sticky");
+      $('#banner').removeClass("banner-sticky");
+      $('.nav-logo').fadeOut(2);
+    }
+  });
+
+
+$('.nav-icon').on('click', () => {
+  $('#nav-menu-mobile').slideToggle(250);
+});
+
+$('.nav-icon').on('click', () => {
+  $('#nav-menu-mobile-video').slideToggle(250);
+});
+
+});
